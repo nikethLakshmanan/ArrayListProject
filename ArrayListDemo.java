8238be25ff908c9d6d9989f703bdf469336554e8
nikethLakshmanan <45395176+nikethLakshmanan@users.noreply.github.com>
2018-11-27 10:35:27 -0500

    Printed it out
    printed new list in the main method by adding print statement in the combine method.

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -7,8 +7,9 @@ public class ArrayListDemo{
 			list1.add(i);
 			list2.add(i);
 		}
+		combineList(list1,list2);
 	}
-	public ArrayList<Integer> combineList(ArrayList<Integer> list1, ArrayList<Integer> list2){
+	public static ArrayList<Integer> combineList(ArrayList<Integer> list1, ArrayList<Integer> list2){
 		ArrayList<Integer> newList= new ArrayList<>();
 		for(int i=0; i<list1.size(); i++){
 			newList.add(list1.get(i));
@@ -16,6 +17,7 @@ public class ArrayListDemo{
 		for(int i=0; i<list2.size(); i++){
 			newList.add(list2.get(i));
 		}
+		System.out.println(newList);
 		return newList;
 	}
 }
