fd8bd803352ccfa9015af3d45706a51204e57561
nikethLakshmanan <45395176+nikethLakshmanan@users.noreply.github.com>
2018-11-27 10:27:56 -0500

    Combine List
    Combined List in new list in the combining method and returned it.

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -3,6 +3,7 @@ public class ArrayListDemo{
 	public static void main(String[]args){
 		ArrayList<Integer> list1 = new ArrayList<>();
 		ArrayList<Integer> list2 = new ArrayList<>();
+		for(int i=1; i<11; i++){
 	}
 	public ArrayList<Integer> combineList(ArrayList<Integer> list1, ArrayList<Integer> list2){
 		ArrayList<Integer> newList= new ArrayList<>();
